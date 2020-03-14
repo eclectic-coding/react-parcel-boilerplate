197183b2a2b4579890b5ad88440d7a55072a43bc
eclectic-coding <chuck@eclecticsaddlebag.com>
2020-03-14 11:57:04 -0400

    Started adding and flushing out a Design System: 1. Added styled componets 2. Themed for priamry and dark mode 3. Global Styles added 4. Working with Buttons, variations, and state.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,52 @@
 import React from 'react'
+import { PrimaryButton, SecondaryButton, TertiaryButton } from './components/UI/Buttons'
+import { GlobalStyle } from './utils/Global'
+import { ThemeProvider } from 'styled-components'
+import { darkTheme, defaultTheme } from './utils'
 
 const App = () => {
+  const [useDarkTheme, setUseDarkTheme] = React.useState(false)
+
   return (
-    <div>
-      <h1>Hello from App.js</h1>
-      <p>Using ParcelJS Bundler</p>
-    </div>
+    <ThemeProvider theme={useDarkTheme ? darkTheme : defaultTheme}>
+      {' '}
+      <GlobalStyle />
+      <button
+        style={{ margin: '0 16px 24px', padding: '8px', background: 'none' }}
+        onClick={() => setUseDarkTheme(true)}
+      >
+        Dark theme
+      </button>
+      <button
+        style={{ margin: '0 16px 24px', padding: '8px', background: 'none' }}
+        onClick={() => setUseDarkTheme(false)}
+      >
+        Default theme
+      </button>
+      <div
+        style={{
+          background: useDarkTheme ? defaultTheme.primaryColor : darkTheme.primaryColor,
+          width: '100vw',
+          height: '90vh',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'space-around'
+        }}
+      >
+        <PrimaryButton modifiers={['small', 'success', 'primaryButtonSuccess']}>
+          {' '}
+          Hello world{' '}
+        </PrimaryButton>
+        <SecondaryButton modifiers={['large', 'warning', 'secondaryButtonWarning']}>
+          {' '}
+          Goodbye world{' '}
+        </SecondaryButton>
+        <TertiaryButton modifiers={['error', 'tertiaryButtonError']}>
+          {' '}
+          Hey world{' '}
+        </TertiaryButton>
+      </div>
+    </ThemeProvider>
   )
 }
 
